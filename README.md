0dec65b1391f277216e03bde7d02905d3a0c1fda
haris <haris.aulia@gmail.com>
2022-08-11 11:50:31 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Geometry Calculator
+
+A simple console based geometry calculator due submission project Binar challenge 01. 
+
+
+## Authors
+
+- [@haris.aulia404](https://gitlab.com/haris.aulia404)
+
---
+++ src/main/java/com/binar/App.java
@@ -0,0 +1,14 @@
+package com.binar;
+
+/**
+ * Created By Harisatul Aulia
+ * 11-8-2022
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
