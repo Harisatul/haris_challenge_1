f126cb38a4bea48fb0937745c13ae7ea4d90a292
haris <haris.aulia@gmail.com>
2022-08-12 15:42:46 +0700

    added bangundatar view

---
+++ src/main/java/com/binar/view/bidangview/PersegiView.java
@@ -0,0 +1,39 @@
+package com.binar.view.bidangview;
+
+import com.binar.entities.bangundatar.Persegi;
+import com.binar.service.luas.HitungLuas;
+import com.binar.service.luas.HitungLuasPersegiImpl;
+import com.binar.view.MenuView;
+
+import java.util.Scanner;
+
+public class PersegiView {
+
+    private HitungLuas luasPersegi;
+    private Persegi persegi;
+
+    public PersegiView() {
+        this.luasPersegi = new HitungLuasPersegiImpl();
+        this.persegi = new Persegi("Persegi");
+    }
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void show(){
+        System.out.println("----------------------------------------------");
+        System.out.println("Anda Memilih " + persegi.getNama());
+        System.out.println("----------------------------------------------");
+        System.out.print("Mausukkan sisi :  ");
+        long sisi = scanner.nextLong();
+        persegi.setSisi(sisi);
+        System.out.println("\nProcessing ....");
+
+        System.out.println("Luas dari persegi panjang adalah " + luasPersegi.hitungLuas(persegi));
+        System.out.print("Ketik apa saja untuk kembali ke main menu : ");
+        scanner.next();
+
+        MenuView menuView = new MenuView();
+        menuView.mainShow();
+    }
+
+}
